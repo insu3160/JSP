1f96494c02914c45ee114cc441013edfc4abb87e
insu3160 <124024198+insu3160@users.noreply.github.com>
2023-02-13 12:30:24 +0900

    Add files via upload

---
+++ src/main/java/GugudanServlet.java
@@ -0,0 +1,25 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/gugudan")
+public class GugudanServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Rq rq = new Rq(req, resp);
+
+        int dan = rq.getIntParam("dan",0);
+        int limit = rq.getIntParam("limit",0);
+
+        resp.getWriter().append("<h1>%d단</h1>".formatted(dan));
+        for (int i = 1; i <= limit; i++) {
+            resp.getWriter().append("<div>%d * %d = %d</div>".formatted(dan,i,dan*i));
+        }
+
+
+    }
+}
